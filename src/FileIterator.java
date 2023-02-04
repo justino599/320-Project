e9dea022e4a97a1d2f26cc44993b3ad39b8a7fc4
justino599 <justin.schoenit@gmail.com>
2023-02-04 14:15:32 -0800

    Initial Commit

---
+++ src/FileIterator.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class FileIterator implements Iterator<String> {
+    private final BufferedReader bufferedReader;
+    private String nextLine;
+
+    public FileIterator(String filename) throws FileNotFoundException {
+        bufferedReader = new BufferedReader(new FileReader(filename));
+        nextLine = getNextString();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return nextLine != null;
+    }
+
+    @Override
+    public String next() {
+        String out = nextLine;
+        nextLine = getNextString();
+        return out;
+    }
+
+    @SuppressWarnings("StringConcatenationInLoop")
+    private String getNextString() {
+        try {
+            String s = bufferedReader.readLine();
+            if (s == null)
+                return null;
+            while (!s.endsWith("\""))
+                s += '\n' + bufferedReader.readLine();
+            return s;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
